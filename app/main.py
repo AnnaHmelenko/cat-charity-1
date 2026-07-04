a83a09d2ebf82957a55c8c826487841daffcec6f
Анна Хмеленко <hmelenkoann@yandex.ru>
2026-07-04 09:52:33 +0300

    ver_1

--- app/main.py
+++ app/main.py
@@ -2,19 +2,11 @@ from fastapi import FastAPI
 
 from app.api.routes import charity_project, donation
 from app.core.config import settings
-from app.core.db import Base, engine
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.app_description,
 )
-
-
-@app.on_event('startup')
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
 
 app.include_router(
     charity_project.router,
